c991948a52f768d6b93ab36db4d87be8dae4e542
rafi <rafi@mail.com>
2022-03-23 09:52:44 +0300

    Queueable Appointment notifications

--- app/Notifications/CreatedAppointmentNotification.php
+++ app/Notifications/CreatedAppointmentNotification.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class CreatedAppointmentNotification extends Notification
+class CreatedAppointmentNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
